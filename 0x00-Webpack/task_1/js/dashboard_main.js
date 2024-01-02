9c0f2b8596e516d517a60041689b5f83a1ac8724
sierra-95 <michaelmachohi@gmail.com>
2024-01-02 19:17:25 +0300

    exact

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,18 @@
-import $ from 'jquery';
+import $ from "jquery";
 import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button type="button">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('button').on('click', _.debounce(updateCounter, 500));
+
 let count = 0;
+
 function updateCounter() {
-	count++;
-	$('#count').text(`${count} clicks on the button`);
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
 }
+
+$('button').on('click', _.debounce(updateCounter, 500));
